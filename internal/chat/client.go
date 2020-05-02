4d2c9b72e7f5feb3ad0cff65aeb4c81754d8fe28
delgus <alexdolgovak47@mail.ru>
2020-05-02 14:21:26 +0300

    ping pong handle

--- internal/chat/client.go
+++ internal/chat/client.go
@@ -2,9 +2,20 @@ package chat
 
 import (
 	"fmt"
-	"io"
+	"time"
 
 	"github.com/gorilla/websocket"
+)
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 30 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
 )
 
 var maxID int
@@ -44,33 +55,57 @@ func (c *Client) Send(msg *Message) {
 
 // Listen read request via channel
 func (c *Client) listenWrite() {
-	for msg := range c.send {
-		err := c.ws.WriteJSON(msg)
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.ws.Close()
+	}()
+	for {
+		select {
+		case msg := <-c.send:
 
-		if err != nil {
-			c.server.Del(c)
-			c.server.Err(err)
-			break
+			c.ws.SetWriteDeadline(time.Now().Add(writeWait))
+
+			err := c.ws.WriteJSON(msg)
+
+			if err != nil {
+				c.server.Del(c)
+				c.server.Err(err)
+				break
+			}
+
+		case <-ticker.C:
+			c.ws.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.ws.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
 		}
 	}
 }
 
 // Listen read request via channel
 func (c *Client) listenRead() {
+	// thx https://stackoverflow.com/questions/37696527/go-gorilla-websockets-on-ping-pong-fail-user-disconnct-call-function
+	c.ws.SetReadDeadline(time.Now().Add(pongWait))
+	c.ws.SetPongHandler(func(string) error {
+		c.ws.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+
 	for {
 		var msg Message
 		err := c.ws.ReadJSON(&msg)
 
-		if err == io.EOF {
+		if err != nil {
 			c.server.Del(c)
+			c.ws.Close()
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+				c.server.Err(err)
+			}
 			break
-		} else if err != nil {
-			c.server.Err(err)
-			c.server.Del(c)
-			break
-		} else {
-			msg.Author = fmt.Sprintf("client_%d", c.id)
-			c.server.broadcast <- &msg
 		}
+
+		msg.Author = fmt.Sprintf("client_%d", c.id)
+		c.server.broadcast <- &msg
 	}
 }
